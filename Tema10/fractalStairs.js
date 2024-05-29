770c38e851b431e88ae6794de024ccd4a14c3cc3
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-05-29 23:21:14 +0300

    second commint

--- Tema10/fractalStairs.js
+++ Tema10/fractalStairs.js
@@ -2,12 +2,11 @@ let w = 100;
 let h = 100
 
 
-
-
 function drawStairs(){
-    translate(500,500)
+    translate(200,200)
     for (i=0;i<=100;i++){
         rotate(QUARTER_PI/10)
+        noFill()
         rect(0,0,w,h);
         w--;
         h--;
